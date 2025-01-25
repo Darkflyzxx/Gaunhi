4782a8afdb3f5268392d35a059ca78874d112f49
Darkflyzxx <128630497+Darkflyzxx@users.noreply.github.com>
2025-01-25 22:38:41 +0700

    Create REansa.lua

---
+++ REansa.lua
@@ -0,0 +1,38 @@
+pcall(function()
+    if not getgenv().Key then
+        getgenv().Key = readfile("Stingray_Key.txt")
+    else
+        writefile("Stingray_Key.txt",getgenv().Key)
+    end
+end)
+
+local Script = [[
+    print("Post request failure")
+]]
+
+local S,E = pcall(function()
+    Script = request({
+        Url = "https://raw.githubusercontent.com/Darkflyzxx/Gaunhi/refs/heads/main/justtest.lua",
+        Headers = {
+            ['Content-Type'] = 'application/json'
+        },
+        Body = game:GetService("HttpService"):JSONEncode({
+            key = tostring(getgenv().Key),
+            hwid = game:GetService("RbxAnalyticsService"):GetClientId(),
+            username = game:GetService("Players").LocalPlayer.Name
+        }),
+        Method = "POST"
+    }).Body
+end)
+
+task.wait(1)
+
+if S then
+    if #Script <= 200 then
+        print(Script)
+    else print(#Script) end
+    writefile("Stingray_JJI.txt",Script)
+    loadstring(Script)()
+else
+    print(E)
+end
